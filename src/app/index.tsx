48284c9da1611c080240892d9fac675ffb5f3daa
Jay-Tiroh <uchechijethro1@gmail.com>
2026-04-09 12:00:25 +0100

    updated db.json

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,27 @@
 import { useState } from "react";
 import { Text, View } from "react-native";
 
+type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  age: number;
+  phone: string;
+  status: "active" | "inactive" | "suspended";
+  address: string;
+};
+
 export default function Index() {
   const [isLoading, setIsLoading] = useState(true);
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState<User[]>([]);
+
+  const getUsers = async () => {
+    try {
+    } catch (err) {
+      console.error("Error fetching users:", err);
+    }
+  };
 
   return (
     <View
